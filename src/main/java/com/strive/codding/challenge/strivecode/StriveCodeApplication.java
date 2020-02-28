9ec344d86597497e139855280edd6d0dcfd43df7
Gulzar <hemani.gulzar@gmail.com>
2020-02-28 11:46:56 -0600

    initial commit

---
+++ src/main/java/com/strive/codding/challenge/strivecode/StriveCodeApplication.java
@@ -0,0 +1,13 @@
+package com.strive.codding.challenge.strivecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriveCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StriveCodeApplication.class, args);
+    }
+
+}
