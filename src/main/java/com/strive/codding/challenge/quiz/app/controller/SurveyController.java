47ef79e5a2a572b78f3efab9323ce2fa96926b2c
Gulzar <hemani.gulzar@gmail.com>
2020-02-28 16:01:07 -0600

    Added cors to the controller

--- src/main/java/com/strive/codding/challenge/quiz/app/controller/SurveyController.java
+++ src/main/java/com/strive/codding/challenge/quiz/app/controller/SurveyController.java
@@ -3,6 +3,7 @@ package com.strive.codding.challenge.quiz.app.controller;
 import com.strive.codding.challenge.quiz.app.model.Survey;
 import com.strive.codding.challenge.quiz.app.repositories.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class SurveyController {
     @Autowired
     private SurveyRepository surveyRepository;
